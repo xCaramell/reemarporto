9ff40303ae3ade554767293347d6c20a31941bd1
rayss <komangdomang12@gmail.com>
2025-07-01 01:46:29 +0700

    Add files via upload

---
+++ src/data/user.js
@@ -0,0 +1,80 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Remmar",
+		name: "Remmar",
+		email: "reemarDev@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/xCaramell",
+		instagram: "https://instagram.com/X",
+	},
+
+	homepage: {
+		title: "Full-stack web and mobile app developer, and informatics student.",
+		description:
+			"I am a backend developer with expertise in React Js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Remmar, I live in Indonesian, informatics student",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "AdaKami.io",
+			description:
+				"AdaKami.io is a responsive web platform for online loan services in Indonesia. It features user registration, loan application, and admin management. Built with a focus on simplicity, security, and mobile-friendly design.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/xCaramell",
+		},
+
+		{
+			title: "Algoritma Py",
+			description:
+				"A collection of Python-based algorithms including sorting, searching, recursion, and basic data structures. Built to strengthen logic and problem-solving skills using clean and efficient code",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/xCaramell",
+		},
+
+		{
+			title: "Html Full Stack",
+			description:
+				"A full stack web application built from scratch using HTML for structure, styled with CSS, dynamic with JavaScript, and connected to a backend using PHP and MySQL. Includes features like user login.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com/xCaramell",
+		},
+
+		{
+			title: "ReactJs student",
+			description:
+				"A modern student management web app built with React.js. It allows users to view, add, edit, and delete student data with a clean and responsive interface. Designed for ease of use and efficient data handling.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/xCaramell",
+		},
+
+		{
+			title: "Website Cafe",
+			description:
+				"A stylish and responsive website for a local café, featuring menu display, contact form, location map, and modern UI design. Built to enhance the café's online presence and customer engagement.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/xCaramell",
+		},
+	],
+};
+
+export default INFO;
